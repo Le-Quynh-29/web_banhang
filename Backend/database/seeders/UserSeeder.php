33c3cf90359e2a8240e2986d223b828839bea4f1
lt-quynh@jit.vn <lt-quynh@jit.vn>
2023-08-23 17:17:39 +0700

    update code

--- Backend/database/seeders/UserSeeder.php
+++ Backend/database/seeders/UserSeeder.php
@@ -30,24 +30,5 @@ class UserSeeder extends Seeder
             'created_at' => Carbon::now()->toDateTimeString(),
             'updated_at' => Carbon::now()->toDateTimeString(),
         ]);
-
-        $faker = Factory::create();
-
-        $limit = 50;
-
-        for ($i = 1; $i <= $limit; $i++) {
-            DB::table('users')->insert([
-                'username' => $faker->firstName,
-                'fullname' => $faker->name,
-                'email' => $faker->email,
-                'gender' => rand(0,1),
-                'role' => rand(1,3),
-                'active' => rand(0,1),
-                'password' => Hash::make('12345678'),
-                'password_raw' => '12345678',
-                'created_at' => Carbon::now()->toDateTimeString(),
-                'updated_at' => Carbon::now()->toDateTimeString(),
-            ]);
-        }
     }
 }
--- Backend/resources/views/layouts/shared/header.blade.php
+++ Backend/resources/views/layouts/shared/header.blade.php
@@ -12,13 +12,22 @@
                         <img class="avatar-img" id="avatar" alt="user@email.com">
                     </div>
                 </a>
-                <div class="dropdown-menu dropdown-menu-end pt-0">
+                <div class="dropdown-menu dropdown-menu-end p-0 ov-hidden">
                     <div class="dropdown-header bg-light py-2">
                         <div class="fw-semibold">{{ auth()->user()->username }}</div>
                     </div>
+                    <a class="dropdown-item" href="javascript:;">
+                        <i class="far fa-user-cog"></i>
+                        Thông tin cá nhân
+                    </a>
+                    <a class="dropdown-item" href="javascript:;">
+                        <i class="far fa-key"></i>
+                        Đổi mật khẩu
+                    </a>
                     <a class="dropdown-item" href="{{ route('logout') }}">
                         <i class="fas fa-sign-out"></i>
-                        Logout</a>
+                        Logout
+                    </a>
                 </div>
             </li>
         </ul>
